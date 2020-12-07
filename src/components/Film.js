e3e4de0793f9d35e142753b744cf029a13b2b05c
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-07 20:05:31 +0000

    film component now working ok and rendering to the browser

---
+++ src/components/Film.js
@@ -0,0 +1,13 @@
+
+const Film = () => {
+
+    return (
+        <li>
+            <p>This is the film</p>
+        </li>
+    );
+
+
+};
+
+export default Film;
--- src/components/FilmReleases.js
+++ src/components/FilmReleases.js
@@ -1,10 +1,13 @@
+import Film from './Film'
+
 
 const FilmReleases = () => {
 
 return (
-    
+    <>
     <h2>This is the film release list</h2>
-    
+    <Film />
+    </>
 );
 };
 
