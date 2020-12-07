90a5eec966159deeae73e2f196f2e194216de723
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-07 19:55:43 +0000

    film container added and rendering to browser

---
+++ src/containers/FilmContainer.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const FilmContainer = () => {
+
+
+    return (
+
+        <h2>This is the main film container</h2>
+        
+    );
+
+
+};
+
+export default FilmContainer;
